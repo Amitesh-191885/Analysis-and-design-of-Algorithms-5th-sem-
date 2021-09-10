3a7384d3c9327b5f926d3a49de0ca0defdba7b64
Amitesh-191885 <amiteshranjan600@gmail.com>
2021-09-10 23:59:11 +0530

    adding clock function to calculate running time

--- Fibonacci Series/FibonacciCode.cpp
+++ Fibonacci Series/FibonacciCode.cpp
@@ -1,9 +1,10 @@
 #include<bits/stdc++.h>
+#include<time.h>
 #include<cmath>
 using namespace std;
 
 int main(){
-  int N=93; // N must be greater than 0;
+  int N=93; // N must be greater than 0
   long long int term1 = 0, term2 = 1;
   try{
     //cin>>N;
@@ -11,6 +12,7 @@ int main(){
       throw runtime_error("N must be greater than '0'. \n");
       exit;
     }
+    clock_t startt = clock(); //storing starting time.
     cout<<term1<<"\n"<<term2<<"\n";
     for(int i=2;i<N;++i){
       long long int term3= term1+term2;
@@ -18,6 +20,8 @@ int main(){
       term1= term2;
       term2= term3;
     }
+    cout<<"\n\n" <<(double)(clock() - startt)/CLOCKS_PER_SEC; //out running time for ans.
+    //end time - start time
   }
   catch (exception& e) {
     cerr << e.what() << endl;
