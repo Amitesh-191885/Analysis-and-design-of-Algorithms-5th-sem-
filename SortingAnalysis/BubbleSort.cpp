a3f15935ad9279c435ea9f218651433556bde1f4
Amitesh-191885 <amiteshranjan600@gmail.com>
2021-09-21 10:34:22 +0530

    BubbleSort

---
+++ SortingAnalysis/BubbleSort.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<time.h>
+using namespace std;
+
+int main(){
+  int N=9,temp;
+  int arr[N] = {2,3,6,8,4,1,5,7,0};
+
+  //Bubble sort Begin
+  clock_t startTime = clock(); //For calculate time difference store starting time
+
+  for(int i=N-1;i>=0;i--){
+    for(int j=0;j<=i-1;j++){ /* This loop for compare value from 0 to i-1, but it compare value upto
+       last element and swap the values. */
+
+      if(arr[j]>arr[j+1]){ 
+        /* Swap the value so that arr is in sorted manner */
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  cout<<"\n"<<(double)(clock() - startTime)/CLOCKS_PER_SEC<<"\n\n";
+  for(int i=0;i<N;i++){
+    cout<<arr[i]<<" ";
+  }
+  return 0;
+}
