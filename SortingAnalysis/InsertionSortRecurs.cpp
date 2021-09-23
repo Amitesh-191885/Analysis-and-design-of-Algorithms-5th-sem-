975fc542b435c410f0faf2bb4198d3f8b27396b7
Amitesh-191885 <amiteshranjan600@gmail.com>
2021-09-23 17:11:21 +0530

    InsertionSort

---
+++ SortingAnalysis/InsertionSortRecurs.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void InsertionSort(int data[],int SizeofArr){
+  //Recursive Insertion Sort
+  
+}
+
+int main(){
+  int DataArr[] = {};
+  int siZe = sizeof(DataArr)/sizeof(DataArr[0]);
+  
+  InsertionSort(DataArr,siZe);
+
+  for(int i=0;i<siZe;i++){
+    cout<<DataArr[i]<<" ";
+  }
+  return 0;
+}
