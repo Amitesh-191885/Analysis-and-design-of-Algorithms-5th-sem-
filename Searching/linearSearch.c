07585bd078e9ad605b03c6fa565f5c511fe444e6
Amitesh-191885 <amiteshranjan600@gmail.com>
2021-12-05 18:59:34 +0530

    code added

--- Searching/linearSearch.c
+++ Searching/linearSearch.c
@@ -2,21 +2,23 @@
 #include<stdlib.h>
 
 int linearsearch(int arr[],int k,int N){
-  if(N<0){
-    return -1;
-  }
-  if(arr[N]==k){
-    return N;
+  if(N>0){
+    if(arr[N]==k){
+      return N;
+    }
+    else{
+      return linearsearch(arr,k,(N-1));
+    }
   }
   else{
-    return linearsearch(arr,k,(N-1));
+    return -1;
   }
 }
 
 int main(){
   int arr[] = {1,3,4,5,12,23,56,21};
   int size = sizeof(arr)/sizeof(arr[0]);
-  int key = 93;
+  int key = 12;
   printf("IF position is -1 means not found\n\n");
   printf("%d found at %d position.\n",key,linearsearch(arr,key,(size-1)));
   return 0;
